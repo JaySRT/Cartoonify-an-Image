3dc59c20043023f947400bfe19c654d9b858db77
JaySRT <48157133+JaySRT@users.noreply.github.com>
2021-01-06 22:21:15 -0500

    Update Source_code.py

--- Source_code.py
+++ Source_code.py
@@ -19,53 +19,67 @@ def upload():
   ImagePath = easygui.fileopenbox()     #fileopenbox() is the method in easyGUI module which returns the path of the chosen file as a string.
   cartoonify(ImagePath)
 
-
 def cartoonify(ImagePath):
-  #read the image
-  originalImage = cv2.imread(ImagePath)
-  originalImage = cv2.cvtColor(originalImage, cv2.COLOR_BGR2RGB)
-  #print(image)  #Image is stored in form of numbers
-  
-  if originalImage is None:
-    print('Can not find any image. Choose approptiate file")
-    sys.exit()
-         
-  Resized1 = cv2. resize(originalImage, (960,540))
-  #plt.imshow(Resized1, cmap = 'grap')
-  
-  #convert an image to grayscale
-  graySaleImage = cv2.cvtColor(originalImage, cv2.COLOR_BGR2GRAY)
-  Resized2 = cv2.resize(grayScaleImage, (960, 540))
-  #plt.imshow(Resized2, cmap = 'gray')
-  
-  #apply median blur to smoothen an image
-  smoothGrayScale = cv2.medianBlur(grayScaleImage, 5)
-  Resized3 = cv2.resize(smoothGrayScale, (960, 540))
-  #plt.imshow(ReSized3, cmap = 'gray')
-  
-  #Retrieve the edges for cartoon effect
-  getEdge = cv2.adaptiveThreshold(smoothGrayScale, 255, cv2.ADAPTIVE_THRESH_MEAN_C, CV2.THRESH_BINARY, 9 ,9)
-  ReSized4 = cv2.ReSize(getEdge, (960, 540))
-  #plt.imshow(ReSized4, cmap='gray')   
-          
-  #applying bilateral filter to remove noise and keep edge sharp as required
-  colorImage = cv2.bilateralFilter(originalImage, 9, 300, 300)
-  ReSized5 = cv2.resize(colorImage, (960, 540))
-  #plt.imshow(ReSized5, cmap='gray')        
-  
-  #Giving a cartoon effect
-  #masking edged image with our "BEAUTIFY" image
-  cartoonImage = cv2.bitwise_and(colorImage, colorImage, mask=getEdge)
-  ReSized6 = cv2.resize(cartoonImage, (960, 540))
-  #plt.imshow(ReSized6, cmap='gray')
-  
-  # Plotting the whole transition
-  images=[ReSized1, ReSized2, ReSized3, ReSized4, ReSized5, ReSized6]
-  fig, axes = plt.subplots(3,2, figsize=(8,8), subplot_kw={'xticks':[], 'yticks':[]}, gridspec_kw=dict(hspace=0.1, wspace=0.1))
-  for i, ax in enumerate(axes.flat):
-      ax.imshow(images[i], cmap='gray')
-  //save button code
-  plt.show()
+    # read the image
+    originalmage = cv2.imread(ImagePath)
+    originalmage = cv2.cvtColor(originalmage, cv2.COLOR_BGR2RGB)
+    #print(image)  # image is stored in form of numbers
+
+    # confirm that image is chosen
+    if originalmage is None:
+        print("Can not find any image. Choose appropriate file")
+        sys.exit()
+
+    ReSized1 = cv2.resize(originalmage, (960, 540))
+    #plt.imshow(ReSized1, cmap='gray')
+
+
+    #converting an image to grayscale
+    grayScaleImage= cv2.cvtColor(originalmage, cv2.COLOR_BGR2GRAY)
+    ReSized2 = cv2.resize(grayScaleImage, (960, 540))
+    #plt.imshow(ReSized2, cmap='gray')
+
+
+    #applying median blur to smoothen an image
+    smoothGrayScale = cv2.medianBlur(grayScaleImage, 5)
+    ReSized3 = cv2.resize(smoothGrayScale, (960, 540))
+    #plt.imshow(ReSized3, cmap='gray')
+
+    #retrieving the edges for cartoon effect
+    #by using thresholding technique
+    getEdge = cv2.adaptiveThreshold(smoothGrayScale, 255, 
+        cv2.ADAPTIVE_THRESH_MEAN_C, 
+        cv2.THRESH_BINARY, 9, 9)
+
+    ReSized4 = cv2.resize(getEdge, (960, 540))
+    #plt.imshow(ReSized4, cmap='gray')
+
+    #applying bilateral filter to remove noise 
+    #and keep edge sharp as required
+    colorImage = cv2.bilateralFilter(originalmage, 9, 300, 300)
+    ReSized5 = cv2.resize(colorImage, (960, 540))
+    #plt.imshow(ReSized5, cmap='gray')
+
+
+    #masking edged image with our "BEAUTIFY" image
+    cartoonImage = cv2.bitwise_and(colorImage, colorImage, mask=getEdge)
+
+    ReSized6 = cv2.resize(cartoonImage, (960, 540))
+    #plt.imshow(ReSized6, cmap='gray')
+
+    # Plotting the whole transition
+    images=[ReSized1, ReSized2, ReSized3, ReSized4, ReSized5, ReSized6]
+
+    fig, axes = plt.subplots(3,2, figsize=(8,8), subplot_kw={'xticks':[], 'yticks':[]}, gridspec_kw=dict(hspace=0.1, wspace=0.1))
+    for i, ax in enumerate(axes.flat):
+        ax.imshow(images[i], cmap='gray')
+
+    save1=Button(top,text="Save cartoon image",command=lambda: save(ReSized6, ImagePath),padx=30,pady=5)
+    save1.configure(background='#364156', foreground='white',font=('calibri',10,'bold'))
+    save1.pack(side=TOP,pady=50)
+    
+    plt.show()
+
   
 #function for SAVE Button
 def save(ReSized6, ImagePath):
@@ -79,6 +93,8 @@ def save(ReSized6, ImagePath):
     tk.messagebox.showinfo(title=None, message=I)
 
           
-          #Not finished yet.
-          
- 
+upload=Button(top,text="Cartoonify an Image",command=upload,padx=10,pady=5)
+upload.configure(background='#364156', foreground='white',font=('calibri',10,'bold'))
+upload.pack(side=TOP,pady=50)
+
+top.mainloop()
